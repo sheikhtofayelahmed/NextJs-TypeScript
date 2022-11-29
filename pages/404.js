b181236c830af6c854562b0f1bd30d173f8fb208
tofayel101 <tofayel101@proton.me>
2022-11-30 01:40:49 +0600

    404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UnknownPage = () => {
+    return (
+        <div>
+            <h1>welcome to the infinity</h1>
+        </div>
+    );
+};
+
+export default UnknownPage;
